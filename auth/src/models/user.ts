a4dae3946b087f4e3f6025216980c9281618e00d
Guillermo Saez <guisaez@email.unc.edu>
2023-02-15 11:25:54 -0500

    Section 8 Checkpoint

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../utils/password";
 
 // An interface that describes the properties that are required 
 // to create a new User.
@@ -26,8 +27,11 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(done) {
     if(this.isModified('password')){
-        
+        const hashed = await Password.toHash(this.get('password'));
+
+        this.set('password', hashed);
     }
+    done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
