92c5f6b82ba9ca2fdcaf96433b6e69cb7b79c665
Guillermo Saez <guisaez@email.unc.edu>
2023-04-02 19:10:40 -0400

    Socket hang up fixed

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,15 +5,13 @@ const buildClient = ({ req }) => {
         // We are on the server
 
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://www.ticketing-app-prod.world/', // 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
             headers: req.headers
         });
-
     }else{
         // We must be on the browser
         return axios.create({
-            baseURL: 'http://www.ticketing-app-prod.world/', // If local you would replace this with '/'
-            headers: req.headers, // not needed with local 
+            baseURL: '/'
         })
     }   
 }
