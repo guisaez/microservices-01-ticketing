72a4b4452e59980bb488e8c9e3baa0fa4ff14bc8
Guillermo Saez <guisaez@email.unc.edu>
2023-04-02 17:37:25 -0400

    Test env

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -10,7 +10,7 @@ jest.mock('../nats-wrapper.ts');
 
 let mongo: any;
 
-process.env.STRIPE_KEY = 'sk_test_51Mr0ytLpkiXGmtRLPbEbUGSrkEuA6nFofleHhznMChl3dKrru37kBvXyOirarzPokyS3uni34JXSkQwmDqXD4s6300dUD8BWbB'
+console.log(process.env.STRIPE_KEY)
 
 beforeAll(async () => {
     process.env.JWT_KEY = 'asdfasdf';
